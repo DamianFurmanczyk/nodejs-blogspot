0dc466b120f9f918cbb851fc5efb2f698c90cda8
Damian <you@example.com>
2017-11-04 21:33:55 +0200

    say hello to my new friend - webpack

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
-require('dotenv/config');
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MLABS);
 
-mongoose
-    .connection
-    .on('error', (err) => {
-        console.error(`☠☠☠☠☠
+mongoose.connection.on("error", err => {
+  console.error(`☠☠☠☠☠
         ☠☠☠☠☠
         ☠☠☠☠☠error occured during connecting to db: \n >${err.message}
         ☠☠☠☠☠
         ☠☠☠☠☠
         ☠☠☠☠☠`);
-    });
+});
 
-require('./models/user');
+require("./models/user");
 
-const app = require('./appConfig');
+const app = require("./appConfig");
 
 const server = app.listen(4200, () => {
-    console.log(`⏩ PORT  ${server.address().port} ⏪`);
-    console.log(`⏩ PORT  ${server.address().port} ⏪`);
-    console.log(`⏩ PORT  ${server.address().port} ⏪`);
+  console.log(`⏩ PORT  ${server.address().port} ⏪`);
+  console.log(`⏩ PORT  ${server.address().port} ⏪`);
+  console.log(`⏩ PORT  ${server.address().port} ⏪`);
 });
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,42 +1,35 @@
-const passport = require('passport');
+const passport = require("passport");
 // const localStr = require('../models/localAuth');
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
 mongoose.Promise = global.Promise;
-const user = mongoose.model('user');
+const user = mongoose.model("user");
 
 exports.gregister = (req, res) => {
-  res.render('register');
+  res.render("register");
 };
 
 exports.valReg = (req, res, next) => {
-  req.sanitizeBody('username');
+  req.sanitizeBody("username");
   console.log(req.body.username);
-  req
-    .checkBody('username', 'username is not valid! ')
-    .notEmpty();
-  req
-    .checkBody('email', 'That email is not valid! ')
-    .isEmail();
-  req.sanitizeBody('email');
+  req.checkBody("username", "username is not valid! ").notEmpty();
+  req.checkBody("email", "That email is not valid! ").isEmail();
+  req.sanitizeBody("email");
 
+  req.checkBody("password", "Password fields cannot be empty!").notEmpty();
   req
-    .checkBody('password', 'Password fields cannot be empty!')
-    .notEmpty();
-  req
-    .checkBody('confirmPassword', 'Password confirmation failed!')
+    .checkBody("confirmPassword", "Password confirmation failed!")
     .equals(req.body.password);
   // req   .checkBody('ispublic', 'Choose user privacy!')   .notEmpty();
 
   const errors = req.validationErrors();
 
   if (errors) {
-    errors
-      .forEach(function (element) {
-        req.flash('error', element.msg);
-      });
+    errors.forEach(function(element) {
+      req.flash("error", element.msg);
+    });
 
-    res.render('register', {
+    res.render("register", {
       body: req.body,
       flashes: req.flash()
     });
@@ -46,35 +39,41 @@ exports.valReg = (req, res, next) => {
 };
 
 exports.register2DB = (req, res, next) => {
-
-  user
-    .register(new user({email: req.body.email, username: req.body.username, ispublic: req.body.ispublic}), req.body.password, function (err, user) {
+  user.register(
+    new user({
+      email: req.body.email,
+      username: req.body.username,
+      ispublic: req.body.ispublic
+    }),
+    req.body.password,
+    function(err, user) {
       if (err) {
-        console.log('errors @ register2DB');
+        console.log("errors @ register2DB");
 
         console.log(err);
 
         const errorKeys = Object.keys(err.errors);
-        errorKeys.forEach(key => req.flash('error', err.errors[key].message));
-        res.redirect('back');
+        errorKeys.forEach(key => req.flash("error", err.errors[key].message));
+        res.redirect("back");
         // if end
       } else {
         return next();
       }
-    });
+    }
+  );
 };
 
-exports.authUser = passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: 'Failed Login!',
-  successRedirect: '/',
-  successFlash: 'You are now logged in!'
+exports.authUser = passport.authenticate("local", {
+  failureRedirect: "/login",
+  failureFlash: "Failed Login!",
+  successRedirect: "/",
+  successFlash: "You are now logged in!"
 });
 
 exports.logout = (req, res) => {
   req.logout();
-  req.flash('success', 'logged out');
-  res.redirect('/');
+  req.flash("success", "logged out");
+  res.redirect("/");
 };
 
 exports.loginM = (req, res, next) => {
@@ -83,13 +82,10 @@ exports.loginM = (req, res, next) => {
 };
 
 exports.login = (req, res) => {
-  res.render('login');
+  res.render("login");
 };
 
 exports.email2lower = (req, res) => {
-  req.body.email = req
-    .body
-    .email
-    .toLowerCase();
+  req.body.email = req.body.email.toLowerCase();
   next();
 };
